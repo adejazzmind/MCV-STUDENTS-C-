759e1af80b839099480ec461be5b96532703e5e3
adejazzmind <adejazzmind@gmail.com>
2025-10-20 16:27:28 +0100

    Add files via upload

---
+++ MathServiceTest.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MvcDemo.Services;
+
+namespace MvcDemo.Tests
+{
+    [TestClass]
+    public class MathServiceTests
+    {
+        private MathService _math;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            _math = new MathService();
+        }
+
+        [TestMethod]
+        public void Add_ReturnsSum()
+        {
+            var result = _math.Add(2, 3);
+            Assert.AreEqual(5, result);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(DivideByZeroException))]
+        public void Divide_ByZero_Throws()
+        {
+            _math.Divide(10, 0);
+        }
+    }
+}
+
